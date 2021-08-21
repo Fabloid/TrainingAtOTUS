2bb0fe37905a78958cfcf8852e71df82d2382d2d
Александр Ильин <alex_ili@mail.ru>
2021-08-21 14:40:06 +0700

    MySerializer

---
+++ MySerializer/Program.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+
+namespace MySerializer {
+    class Program {
+        static void Main(string[] args) {
+            TestClass testClass = new TestClass {
+                i1 = 1,
+                i2 = 2,
+                i3 = 3,
+                i4 = 4,
+                i5 = 5
+            };
+            int iterations = 100000;
+
+            Console.WriteLine("Время выполнения моей сериализации - " + MySerialization(testClass, iterations));
+            Console.WriteLine("Время выполнения моей десериализации - " + MyDeserialization(testClass, iterations));
+            Console.WriteLine();
+            Console.WriteLine("Время выполнения Newtonsoft сериализации - " + NewtonsoftJsonSerialization(testClass, iterations));
+            Console.WriteLine("Время выполнения Newtonsoft десериализации - " + NewtonsoftJsonDeserialization(testClass, iterations));
+        }
+
+        private static string MySerialization(TestClass testClass, int iterations) {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < iterations; i++) {
+                string csv = Serializer<TestClass>.Serialization(testClass);
+            }
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            return string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+        }
+
+        private static string MyDeserialization(TestClass testClass, int iterations) {
+            string csv = Serializer<TestClass>.Serialization(testClass);
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < iterations; i++) {
+                TestClass deserializedClass = Serializer<TestClass>.Deserialization(csv);
+            }
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            return string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+        }
+
+        private static string NewtonsoftJsonSerialization(TestClass testClass, int iterations) {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < iterations; i++) {
+                string json = JsonConvert.SerializeObject(testClass);
+            }
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            return string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+        }
+
+        private static string NewtonsoftJsonDeserialization(TestClass testClass, int iterations) {
+            string json = JsonConvert.SerializeObject(testClass);
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < iterations; i++) {
+                TestClass deserializedClass = JsonConvert.DeserializeObject<TestClass>(json);
+            }
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            return string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+        }
+    }
+
+    public class TestClass {
+        public int i1, i2, i3, i4, i5;
+    }
+}
---
+++ MySerializer/Serializer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySerializer {
+    public static class Serializer<T> where T : new() {
+        private static char Separator { get; set; } = ';';
+
+        public static string Serialization(T obj) {
+            var type = typeof(T);
+            PropertyInfo[] properties = type.GetProperties();
+            FieldInfo[] fields = type.GetFields();
+
+
+            var sb = new StringBuilder();
+            var values = new List<string>();
+
+            sb.AppendLine(GetHeader(properties, fields));
+            values.Clear();
+
+            foreach (var p in properties) {
+                var raw = p.GetValue(obj);
+                var value = raw == null ?
+                    "" :
+                    raw.ToString();
+                values.Add(value);
+            }
+
+            foreach (var f in fields) {
+                var raw = f.GetValue(obj);
+                var value = raw == null ?
+                    "" : raw.ToString();
+                values.Add(value);
+            }
+
+            sb.AppendLine(string.Join(Separator.ToString(), values.ToArray()));
+
+            return sb.ToString().Replace(Environment.NewLine, "`").Trim('`').Replace("`", Environment.NewLine);
+        }
+
+        private static string GetHeader(PropertyInfo[] _properties, FieldInfo[] _fields) {
+            var properties = _properties.Select(a => a.Name).ToArray();
+            var fields = _fields.Select(a => a.Name).ToArray();
+
+            var header = string.Concat(properties.Any() ?
+                string.Join(Separator.ToString(), properties) + ';' : string.Empty,
+                fields.Any() ?
+                string.Join(Separator.ToString(), fields) : string.Empty);
+            return header;
+        }
+
+        public static T Deserialization(string csv) {
+            List<string> csvLine = csv.Split(Environment.NewLine, StringSplitOptions.None).ToList();
+            string[] columns = csvLine[0].Split(Separator);
+            csvLine.RemoveAt(0);
+            string[] rows = csvLine.ToArray();
+
+            T data = new();
+
+            var type = typeof(T);
+            for (int row = 0; row < rows.Length; row++) {
+                var line = rows[row];
+
+                var parts = line.Split(Separator);
+                for (int i = 0; i < parts.Length; i++) {
+                    var value = parts[i];
+                    var column = columns[i];
+
+                    if (type.GetProperties().Any()) {
+                        var p = type.GetProperties().First(f => f.Name == column);
+
+                        var converter = TypeDescriptor.GetConverter(p.PropertyType);
+                        var convertedvalue = converter.ConvertFrom(value);
+
+                        p.SetValue(data, convertedvalue);
+                    }
+
+                    if (type.GetFields().Any()) {
+                        var f = type.GetFields().First(f => f.Name == column);
+
+                        var converter = TypeDescriptor.GetConverter(f.FieldType);
+                        var convertedvalue = converter.ConvertFrom(value);
+
+                        f.SetValue(data, convertedvalue);
+                    }
+                }
+            }
+
+            return data;
+        }
+    }
+}
