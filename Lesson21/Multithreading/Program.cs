eb839399b59f6dbf28a7d84ba8bd83f54bbd70d5
Александр Ильин <alex_ili@mail.ru>
2022-01-31 15:11:58 +0700

    Lesson21

---
+++ Lesson21/Multithreading/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Multithreading {
+    class Program {
+        static void Main(string[] args) {
+            int count = 0;
+            do {
+                try {
+                    Console.Write("Введите колличество элементов массива, либо 0 для выхода: ");
+                    count = Convert.ToInt32(Console.ReadLine());
+                    Summator summator = new Summator(count);
+                    Console.WriteLine($"Обычное суммирование {count} элементов за {summator.SumTime}мс. Сумму = {summator.Result}");
+                    Console.WriteLine($"Параллельное суммирование {count} элементов за {summator.ThreadSumTime}мс. Сумму = {summator.Result}");
+                    Console.WriteLine($"Параллельное с помощью LINQ суммирование {count} элементов за {summator.PLINQSumTime}мс. Сумму = {summator.Result}");
+                } catch (Exception ex) {
+                    Console.Write(ex.Message);
+                }
+            } while (count != 0);
+        }
+    }
+}
---
+++ Lesson21/Multithreading/Summator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multithreading {
+    public class Summator {
+        private int _length;
+        private int[] _array;
+        private Stopwatch _sp;
+
+        public Summator(int length) {
+            _length = length;
+            Random random = new Random();
+            _sp = new Stopwatch();
+            _array = new int[_length];
+            for (int i = 0; i < _length; i++) {
+                _array[i] = random.Next(-10,10);
+            }
+        }
+
+        private int Sum() {
+            int sum = 0;
+            foreach (var item in _array) {
+                sum += item;
+            }
+            return sum;
+        }
+
+        private int ThreadSum(int threadCount) {
+            int sum = 0;
+
+            var count = _array.Length / threadCount;
+            Task[] tasks = new Task[threadCount];
+            for (int iThread = 0; iThread < threadCount; iThread++) {
+                var localThread = iThread;
+                tasks[localThread] = Task.Run(() => {
+                    for (int j = localThread * count; j < (localThread + 1) * count; j++) {
+                        Interlocked.Add(ref sum, _array[j]);
+                    }
+                });
+            }
+
+            Task.WaitAll(tasks);
+            return sum;
+        }
+
+        private int PLINQSum() {
+            return _array.AsParallel().Sum();
+        }
+
+        public int Result;
+
+        public long SumTime {
+            get {
+                _sp.Reset();
+                _sp.Start();
+                Result = Sum();
+                _sp.Stop();
+                return _sp.ElapsedMilliseconds;
+            }
+        }
+
+        public long ThreadSumTime {
+            get {
+                _sp.Reset();
+                _sp.Start();
+                Result = ThreadSum(4);
+                _sp.Stop();
+                return _sp.ElapsedMilliseconds;
+            }
+        }
+
+        public long PLINQSumTime {
+            get {
+                _sp.Reset();
+                _sp.Start();
+                Result = PLINQSum();
+                _sp.Stop();
+                return _sp.ElapsedMilliseconds;
+            }
+        }
+    }
+}
